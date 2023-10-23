791787e6787f5f7c78d9afbc986dc1d9de571dde
setyp <river60cbc@gmail.com>
2023-10-23 11:08:34 +0700

    main

---
+++ src/pages/Jurusan.js
@@ -0,0 +1,12 @@
+import { container, row, col} from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+function Jurusan() {
+    return(
+        <container>
+            <col1><h2>Data jurusan</h2></col1>
+        </container>
+    )
+}
+
+export default Jurusan;
